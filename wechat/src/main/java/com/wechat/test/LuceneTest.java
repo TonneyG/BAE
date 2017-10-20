87170f60cafb08efbc655c7efcfb89ef85ece691
unknown <17080127@sn.suning.ad>
2017-10-20 14:20:13 +0800

    commit company pc

--- wechat/src/main/java/com/wechat/test/LuceneTest.java
+++ wechat/src/main/java/com/wechat/test/LuceneTest.java
@@ -8,6 +8,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -35,6 +36,11 @@ public class LuceneTest {
 		indexWriter.commit();
 		indexWriter.close();
 		directory.close();
+	}
+	
+	public void search(Analyzer analyzer,String key){
+		DirectoryReader ireader = DirectoryReader.open(directory);
+		
 	}
 	
 	public void deleteFile(File file){
